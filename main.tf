9ca12d53c29cb1c320931c8a9d7d6a99d5c76c73
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2024-11-11 16:11:19 +0530

    comments added

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
-
+#Network
 resource "civo_network" "custom_network" {
   label = "custom-network"
-
 }
 
 
-# Define a firewall for the Kubernetes cluster
+# Firewall
 resource "civo_firewall" "my_firewall" {
   name                 = "my-firewall"
   create_default_rules = true
@@ -13,20 +12,19 @@ resource "civo_firewall" "my_firewall" {
   depends_on           = [civo_network.custom_network]
 }
 
-# Define the Kubernetes cluster
+# Kubernetes cluster
 resource "civo_kubernetes_cluster" "my_cluster" {
   name        = "my-cluster"
-  firewall_id = civo_firewall.my_firewall.id # Reference the firewall ID here
+  firewall_id = civo_firewall.my_firewall.id
   network_id  = civo_network.custom_network.id
   pools {
     node_count = 2
     size       = "g4s.kube.small"
   }
-
   depends_on = [civo_firewall.my_firewall] # Ensure the firewall is created first
 }
 
-# Output kubeconfig as a string
+# Confirmation
 output "kubeconfig" {
   value     = civo_kubernetes_cluster.my_cluster.kubeconfig
   sensitive = true
